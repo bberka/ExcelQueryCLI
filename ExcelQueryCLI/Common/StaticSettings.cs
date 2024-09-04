9e5ff8f76143bd0b7ec69e62790cafb76e3fcddd
Berkay <berkaybyr@icloud.com>
2024-09-04 13:42:30 +0300

    fix: StaticSettings.cs parallel threads default count

--- ExcelQueryCLI/Common/StaticSettings.cs
+++ ExcelQueryCLI/Common/StaticSettings.cs
@@ -1,12 +1,10 @@
-﻿using ExcelQueryCLI.Static;
-
-namespace ExcelQueryCLI.Common;
+﻿namespace ExcelQueryCLI.Common;
 
 public static class StaticSettings
 {
   public const int DefaultHeaderRowNumber = 1;
   public const int DefaultStartRowIndex = 2;
-  public const int DefaultParallelThreads = -1;
+  public const int DefaultParallelThreads = 1;
   public const bool DefaultOnlyFirst = false;
 
   public const bool DefaultBackup = false;
