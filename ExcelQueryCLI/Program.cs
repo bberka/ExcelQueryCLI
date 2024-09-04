0aa3d8b5049bd37cf3e8c0393a0e7628e8db7620
Berkay <berkaybyr@icloud.com>
2024-09-04 17:19:28 +0300

    Program.cs cleanup

--- ExcelQueryCLI/Program.cs
+++ ExcelQueryCLI/Program.cs
@@ -1,19 +1,4 @@
 ﻿using Cocona;
 using ExcelQueryCLI;
 
-// Log.Logger = new LoggerConfiguration()
-//              .MinimumLevel.Information()
-//              .WriteTo.Console()
-//              .WriteTo.File(new CompactJsonFormatter(), "logs/log.json", rollingInterval: RollingInterval.Hour)
-//              .CreateLogger();
-// var version = typeof(Program).Assembly.GetName().Version;
-// Log.Information("ExcelQueryCLI v{version}", version);
-
-// #if DEBUG
-// args = ["update", "-q", @"D:\test.xml"];
-// #endif
-
-
-var app = CoconaApp.Create();
-
 CoconaApp.Run<ExcelQueryCoconaApp>(args);
